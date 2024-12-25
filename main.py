090056e3dadfa2a408f201e0be09805baf1ad858
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2024-12-25 09:12:55 -0700

    put logic in an if-main statement

--- main.py
+++ main.py
@@ -8,34 +8,6 @@ from numba_pokemon_prngs.data.personal import PERSONAL_INFO_LA, PersonalInfo8LA
 import pla_reverse.pla_reverse as pla_reverse
 from pa8 import PA8
 
-SHINY_LOCKED = False
-POKEMON: PA8 = np.fromfile("pokemon.pa8", dtype=PA8.dtype).view(np.recarray)[0]
-PERSONAL_INFO: PersonalInfo8LA = PERSONAL_INFO_LA[POKEMON.species]
-if POKEMON.form:
-    PERSONAL_INFO: PersonalInfo8LA = PERSONAL_INFO_LA[
-        PERSONAL_INFO.form_stats_index + POKEMON.form - 1
-    ]
-POKEMON_IVS = (
-    POKEMON.iv32 & 0x1F,
-    (POKEMON.iv32 >> 5) & 0x1F,
-    (POKEMON.iv32 >> 10) & 0x1F,
-    (POKEMON.iv32 >> 20) & 0x1F,
-    (POKEMON.iv32 >> 25) & 0x1F,
-    (POKEMON.iv32 >> 15) & 0x1F,
-)
-MAX_GUARANTEED_IVS = POKEMON_IVS.count(31)
-IS_SHINY = (
-    (POKEMON.pid & 0xFFFF) ^ (POKEMON.pid >> 16) ^ POKEMON.tid ^ POKEMON.sid
-) < 16
-IS_ALPHA = POKEMON._16 & 32
-
-LEGAL_SHINY_ROLLS = (
-    range(1, 33)
-    if IS_SHINY
-    else (1, 2, 4, 5, 7, 13, 14, 16, 17, 19, 26, 27, 29, 30, 32)
-)
-
-
 def xoroshiro128plus_next(
     seed0: np.uint64, seed1: np.uint64
 ) -> tuple[np.uint64, np.uint64]:
@@ -226,35 +198,62 @@ def verify_all_seeds(seeds, pokemon_pid, tsv, shiny_rolls, shiny_locked):
         bools[i] = pid == pokemon_pid
     return bools
 
-
-for shiny_rolls in LEGAL_SHINY_ROLLS:
-    seeds = find_fixed_seeds_ec_pid(
-        POKEMON.encryption_constant,
-        POKEMON.pid,
-        POKEMON.tid ^ POKEMON.sid,
-        shiny_rolls,
-        SHINY_LOCKED,
+if __name__ == "__main__":
+    SHINY_LOCKED = False
+    POKEMON: PA8 = np.fromfile("pokemon.pa8", dtype=PA8.dtype).view(np.recarray)[0]
+    PERSONAL_INFO: PersonalInfo8LA = PERSONAL_INFO_LA[POKEMON.species]
+    if POKEMON.form:
+        PERSONAL_INFO: PersonalInfo8LA = PERSONAL_INFO_LA[
+            PERSONAL_INFO.form_stats_index + POKEMON.form - 1
+        ]
+    POKEMON_IVS = (
+        POKEMON.iv32 & 0x1F,
+        (POKEMON.iv32 >> 5) & 0x1F,
+        (POKEMON.iv32 >> 10) & 0x1F,
+        (POKEMON.iv32 >> 20) & 0x1F,
+        (POKEMON.iv32 >> 25) & 0x1F,
+        (POKEMON.iv32 >> 15) & 0x1F,
     )
-    for seed in seeds:
-        for guaranteed_ivs in range(MAX_GUARANTEED_IVS + 1):
-            (
-                shiny,
-                encryption_constant,
-                pid,
-                ivs,
-                ability,
-                gender,
-                nature,
-                height,
-                weight,
-            ) = generate_fix_init_spec(
-                seed,
-                PERSONAL_INFO.gender_ratio,
-                shiny_rolls,
-                SHINY_LOCKED,
-                guaranteed_ivs,
-                IS_ALPHA,
-                POKEMON.tid | (POKEMON.sid << np.uint32(16)),
-            )
-            if all(iv0 == iv1 for iv0, iv1 in zip(POKEMON_IVS, ivs)):
-                print(f"Matches {guaranteed_ivs=} | {shiny_rolls=} | {seed=:016X}")
+    MAX_GUARANTEED_IVS = POKEMON_IVS.count(31)
+    IS_SHINY = (
+        (POKEMON.pid & 0xFFFF) ^ (POKEMON.pid >> 16) ^ POKEMON.tid ^ POKEMON.sid
+    ) < 16
+    IS_ALPHA = POKEMON._16 & 32
+
+    LEGAL_SHINY_ROLLS = (
+        range(1, 33)
+        if IS_SHINY
+        else (1, 2, 4, 5, 7, 13, 14, 16, 17, 19, 26, 27, 29, 30, 32)
+    )
+
+    for shiny_rolls in LEGAL_SHINY_ROLLS:
+        seeds = find_fixed_seeds_ec_pid(
+            POKEMON.encryption_constant,
+            POKEMON.pid,
+            POKEMON.tid ^ POKEMON.sid,
+            shiny_rolls,
+            SHINY_LOCKED,
+        )
+        for seed in seeds:
+            for guaranteed_ivs in range(MAX_GUARANTEED_IVS + 1):
+                (
+                    shiny,
+                    encryption_constant,
+                    pid,
+                    ivs,
+                    ability,
+                    gender,
+                    nature,
+                    height,
+                    weight,
+                ) = generate_fix_init_spec(
+                    seed,
+                    PERSONAL_INFO.gender_ratio,
+                    shiny_rolls,
+                    SHINY_LOCKED,
+                    guaranteed_ivs,
+                    IS_ALPHA,
+                    POKEMON.tid | (POKEMON.sid << np.uint32(16)),
+                )
+                if all(iv0 == iv1 for iv0, iv1 in zip(POKEMON_IVS, ivs)):
+                    print(f"Matches {guaranteed_ivs=} | {shiny_rolls=} | {seed=:016X}")
